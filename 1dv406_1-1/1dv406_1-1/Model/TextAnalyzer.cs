f1762271080ce91f989f9acbbbe701bca0d632fa
Vivi <vivilam@hotmail.com>
2014-01-22 19:04:23 +0100

    Förhoppningsvis syns denna modelkatalog nu

---
+++ 1dv406_1-1/1dv406_1-1/Model/TextAnalyzer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _1dv406_1_1.Model
+{
+    public static class TextAnalyzer
+    {
+        public static int GetNumberOfCapitals(string text) 
+        {
+            int onlyUpperCase = 0;
+
+            foreach(char c in text)
+            {
+                onlyUpperCase++;
+            }
+            return onlyUpperCase;
+        }
+    }
+}
