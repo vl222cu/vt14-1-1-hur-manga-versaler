62869e399f165c1421d5a35ed7e59c577e1f9a6c
Vivi <vivilam@hotmail.com>
2014-01-22 20:46:19 +0100

    Missade Char.IsUpper helt men nu så :)

--- 1dv406_1-1/1dv406_1-1/Model/TextAnalyzer.cs
+++ 1dv406_1-1/1dv406_1-1/Model/TextAnalyzer.cs
@@ -7,13 +7,17 @@ namespace _1dv406_1_1.Model
 {
     public static class TextAnalyzer
     {
+        // Metod som returnerar antalet versaler som finns i strängen
         public static int GetNumberOfCapitals(string text) 
         {
             int onlyUpperCase = 0;
 
             foreach(char c in text)
             {
-                onlyUpperCase++;
+                if (Char.IsUpper(c)) 
+                {
+                    onlyUpperCase++;
+                }
             }
             return onlyUpperCase;
         }
